c49f1e632e4367cc94559a5cee86dcca0397813f
Varsha Devaiah <cdvarsha1@gmail.com>
2023-12-12 15:49:36 +0530

    my first commint

---
+++ src/com/javaproject/empapp/DBConnection.java
@@ -0,0 +1,26 @@
+package com.javaproject.empapp;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    static Connection con;
+    public static Connection createDBConnetion(){
+
+        try{
+            //load driver
+            Class.forName("com.mysql.jdbc.Driver");
+            //get connection
+            String url="jdbc:mysql://localhost:3306/employeeDB?useSSL=false";
+            String username="root";
+            String password="Varsha@06";
+            con= DriverManager.getConnection(url,username,password);
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return con;
+
+    }
+}
